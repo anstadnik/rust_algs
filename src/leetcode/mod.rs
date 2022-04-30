82d0fde5dee692f05cf424cd20c99366c2c4aa9d
Stadnik Andrii <stadnikandriy1@gmail.com>
2022-04-30 08:35:10 +0300

    Remove prelude

--- src/leetcode/mod.rs
+++ src/leetcode/mod.rs
@@ -1,7 +1,2 @@
-mod list_node;
-mod tree_node;
-
-pub mod prelude {
-    pub use super::list_node::{link2str, str2list, ListNode};
-    pub use super::tree_node::{str2tree, tree2str, TreeNode};
-}
+pub mod list_node;
+pub mod tree_node;
